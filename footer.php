6a2c300c6e1c000fbc3c5ead8ad0564440c8076d
rwsf <128382666+cckxph@users.noreply.github.com>
2026-04-19 22:11:48 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <hr>
     <!-- Copyright information -->
     <small>© Assignment System</small>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </footer>
 </div>
 </body>
