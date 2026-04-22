49ae5d88aff76029d8f777d63ee4636cdd018e89
rwsf <128382666+cckxph@users.noreply.github.com>
2026-04-22 21:35:18 +0800

    Change name

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+// Connect to database
+$conn = new mysqli("localhost", "root", "", "assignment_system");
+
+// Check database connection
+// If connection fails, stop the system and show error
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Start session (used for login system and user data)
+// session_start();
+?>
