abdc6aa90288fb75cf62ef2531094d9de7e399ee
AivanArthur4 <aivanarthur@gmail.com>
2026-04-23 12:22:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 // Start session
 session_start();
 
-// If already logged in → go dashboard
+// if logged in,go to dashboard
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
     exit();
 }
 
-// If not logged in → go login
+// not logged in,go to login page
 header("Location: login.php");
 exit();
 ?>
